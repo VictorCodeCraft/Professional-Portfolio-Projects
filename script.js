be37ee3e995c2204e1db696dd430f2eee98b3def
Victor Guilherme <guilhermesantana1240@gmail.com>
2024-05-14 16:23:49 -0300

    script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Desenvolvedor Back-End Trainee', 'Estudante de Ciência da Computação' ],
+    strings: ['Desenvolvedor', 'Estudante de Analise e Desenvolvimento de Sistemas' ],
     typedSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
